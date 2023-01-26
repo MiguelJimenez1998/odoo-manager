6ca694de2405f42f07df6859d1fb15df3ed71d03
Miguel Jimenez <elkin1198@hotmail.com>
2023-01-26 02:35:35 -0500

    fix installer

--- installer.sh
+++ installer.sh
@@ -14,8 +14,8 @@ echo 'odoo ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/odoo
 # 3. Odoo task
 sudo cp -r templates /home/odoo
 sudo cp -r scripts /home/odoo
-sudo chown odoo:odoo /home/odoo/templates
-sudo chown odoo:odoo /home/odoo/scripts
+sudo chown -R odoo:odoo /home/odoo/templates
+sudo chown -R odoo:odoo /home/odoo/scripts
 
 ls scripts | while read script 
 do
